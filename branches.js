4587ae3349774b2e2230ed429dfb20d0adfbc15a
zwen0504 <zwen0504@uni.sydney.edu.au>
2025-06-10 00:43:42 +1000

    Changed ball generation logic
    Made sure each ball is connected with the next

--- branches.js
+++ branches.js
@@ -1,12 +1,14 @@
+//Empty array of balls 
 let balls = []
 
-
+//Setup function to generate start position of trunk
 function setup() {
   createCanvas(1200, 1200)
   let trunk = new Circles(width/2, height - height/3, 50)
   trunk.generateTrunk()
 }
 
+//Draw function for display ball function
 function draw() {
   background(222)
 
@@ -15,13 +17,15 @@ function draw() {
     }
 }
 
+//Circles class storing information of each ball
 class Circles {
 
-  constructor(starterXPos, starterYPos, starterRadius){
+  constructor(starterXPos, starterYPos, starterDiameter){
     this.ballXPos = starterXPos
     this.ballYPos = starterYPos
-    this.ballRadius = starterRadius
+    this.ballDiameter = starterDiameter
     
+    //stored random colors with min/max of browns for trunk
     this.colorTop = color(random(60, 100), random(40, 70), random(20, 50));
     this.colorBottom = color(random(100, 150), random(60, 100), random(30, 80));
 
@@ -29,15 +33,22 @@ class Circles {
     this.StrokeWeight = 0
   }
   
+  //generate trunk function
   generateTrunk() {
     const segments = 5
-    let y = this.ballYPos
+    let y = this.ballYPos 
     let x = this.ballXPos
+    let prevD = this.ballDiameter
+    
+    balls.push(this)
 
     for (let i=0; i<segments; i++){
-      let r = random(20, 100)
-      balls.push(new Circles(x, y, r))
-      y -= r
+      let newD = Math.round(random(20, 100))
+      y -= (prevD/2 + newD/2)
+      balls.push(new Circles(x, y, newD))
+      prevD = newD
+
+  
     }
   }
 
@@ -52,10 +63,10 @@ class Circles {
     strokeWeight(this.StrokeWeight)
 
     fill(this.colorTop)
-    arc(0,0, this.ballRadius, this.ballRadius, PI/2, (3*PI)/2, PIE)
+    arc(0,0, this.ballDiameter, this.ballDiameter, PI/2, (3*PI)/2, PIE)
 
     fill(this.colorBottom)
-    arc(0,0, this.ballRadius, this.ballRadius, (3*PI)/2, PI/2)
+    arc(0,0, this.ballDiameter, this.ballDiameter, (3*PI)/2, PI/2)
 
 
     pop()
